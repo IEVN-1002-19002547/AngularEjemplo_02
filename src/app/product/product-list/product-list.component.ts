4f92ad0480fe57c31bb898fdeb5fc629f96f84b0
Francisco Guerrero <francisco.ontiveros971@gmail.com>
2023-09-21 18:51:55 -0600

    ----21/09/2023 GUOF----

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+
+  imageWidth:number=50;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string='';
+
+  productos:any[]=[
+    {
+      "productoId":1,
+      "Modelo":"Sentra",
+      "Descripcion":"Dos puertas",
+      "year":"Marzo 5, 2022",
+      "precio":120000,
+      "Marca":"Nissan",
+      "Color":"Gris Metalico",
+      "ImageUrl":"https://www.nissancelayarenacimiento.com.mx/inventoryphotos/9614/9bwdl5bz1np006086/ip/1.jpg"
+    },
+    {
+      "productoId":2,
+      "Modelo":"A4",
+      "Descripcion":"Cuatro puertas familiar",
+      "year":"Febrero 3, 2020",
+      "precio":180000,
+      "Marca":"AUDI",
+      "Color":"Blanco",
+      "ImageUrl":"https://d2v9mob6nwdg55.cloudfront.net/vo/imagenes/h_img_6_18246_01_1693107237.jpg"
+    },
+    {
+      "productoId":3,
+      "Modelo":"Rio",
+      "Descripcion":"Dos puertas familiar",
+      "year":"Enero 2, 2023",
+      "precio":200000,
+      "Marca":"Kia",
+      "Color":"Azul Marino",
+      "ImageUrl":"https://phantom-marca.unidadeditorial.es/ebeac71437aeb79312db1ab6b78f1f95/crop/0x152/2046x1300/resize/1200/f/jpg/assets/multimedia/imagenes/2021/01/21/16112406024829.jpg"
+    }
+  ]
+}
